d39c9dbf72f4de2b5220362a6effbb0646431403
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-02-11 11:58:56 +0530

    carousel button controls

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ else{
     navbar.classList.remove('scrolled');
 }
 };
+
+const initSlider = () => {
+    const imageList = document.querySelector(".carousel .movie");
+    const sliderButtons = document.querySelectorAll(".carousel .carousel-button");
+    sliderButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            button.addEventListener("click", () => {
+                const direction = button.id === "prev-slide" ? -1 : 1 
+                const scrollAmount = imageList.clientWidth * direction
+                imageList.scrollBy({left: scrollAmount, behavior: "smooth"})
+            })
+        })
+    })
+}
+
+window.addEventListener("load", initSlider)
